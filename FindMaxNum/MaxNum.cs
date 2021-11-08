12c235f302575d0ea94d141630e28291ffe73d26
Ameejac <ameejacherukuru123@gmail.com>
2021-11-08 23:25:00 +0530

    ameeja[added:in three integers to find the maximum number]

---
+++ FindMaxNum/MaxNum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaxNum
+{
+    class MaxNum
+    {
+        public static  int Find3MaxNum(int firstvalue,int secondvalue,int thirdvalue)
+        {
+            if(firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0)
+            {
+                return firstvalue;
+            }
+            else if(secondvalue.CompareTo(firstvalue) > 0 &&secondvalue.CompareTo(thirdvalue) > 0)
+            {
+                return secondvalue;
+            }
+            else if(thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0)
+            {
+                return thirdvalue;
+            }
+            else
+            {
+                Console.WriteLine("two are more numbers are equal");
+            }
+            return 0;
+        }
+    }
+}
---
+++ FindMaxNum/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FindMaxNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+           
+            Console.WriteLine("the maximum value is:"+MaxNum.Find3MaxNum(10,15,40));
+            
+        }
+    }
+}
