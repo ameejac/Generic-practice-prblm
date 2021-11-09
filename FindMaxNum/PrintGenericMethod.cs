2aace68f3303620a179f2a569c90b6ff8bc2db9d
Ameejac <ameejacherukuru123@gmail.com>
2021-11-09 12:39:42 +0530

    ameeja[added:extend the maximum method and print max using generic method]

---
+++ FindMaxNum/PrintGenericMethod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaxNum
+{
+    class PrintGenericMethod<E>
+    {
+        public E[] array;
+        public PrintGenericMethod(E[] array)
+        {
+            this.array = array;
+        }
+        public void Toprint()
+        {
+            Console.WriteLine("displaying the array elements");
+            foreach(var element in array)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
